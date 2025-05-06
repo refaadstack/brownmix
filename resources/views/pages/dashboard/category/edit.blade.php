1983518f370496407b2e15921b6f51fd23e8ab3e
refaadstack <refaad16@gmail.com>
2025-05-06 10:48:45 +0700

    fix: form category

--- resources/views/pages/dashboard/category/edit.blade.php
+++ resources/views/pages/dashboard/category/edit.blade.php
@@ -30,7 +30,7 @@
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">Nama</label>
-                            <input type="text" value="{{old('name') ?? $category->name}}" name="nama" placeholder="Nama" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" autocomplete="off">
+                            <input type="text" value="{{old('name') ?? $category->name}}" name="name" placeholder="Nama" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" autocomplete="off">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
